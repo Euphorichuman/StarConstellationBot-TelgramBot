23ded5b4b547863dedfece53f2613ec724324f61
Martin Fuentes <mefuentes@uninorte.edu.co>
2020-05-23 12:38:11 -0500

    Se define la función "main"

---
+++ main.py
@@ -0,0 +1,8 @@
+from config import TOKEN
+from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+#from telegram import 
+import bot
+# @nombre de usuario de nuestro bot
+
+def main():
+    #argumentos 
